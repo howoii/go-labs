9b22b694057042f1d84f7651dcd80a733ca421f2
howaii <18868119485@163.com>
2022-01-19 11:37:09 +0800

    test closure var

--- tests/closure_test.go
+++ tests/closure_test.go
@@ -6,15 +6,28 @@ import (
 	"testing"
 )
 
+// result:
+// 1: 2
+// 2: 1
 func TestClosureVariable(t *testing.T) {
 	v := 1
+	begin := make(chan int)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
-		wg.Wait()
-		fmt.Println(v)
+		<-begin
+		fmt.Printf("1: %d\n", v)
+		wg.Done()
 	}()
 
+	wg.Add(1)
+	go func(i int) {
+		<-begin
+		fmt.Printf("2: %d\n", i)
+		wg.Done()
+	}(v)
+
 	v = 2
-	wg.Done()
+	close(begin)
+	wg.Wait()
 }
